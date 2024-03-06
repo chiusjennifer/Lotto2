e145b6a5f0158dc67884c9ce05678f8261d53fdf
chiusjennifer <chiusjennifer@gmail.com>
2024-03-06 17:44:58 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/csce/s1114859/lotto2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csce/s1114859/lotto2/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.edu.pu.csce.s1114859.lotto2
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv: TextView =findViewById(R.id.txv)
         txv.text="0"
+    }
+
+    fun happy(v: View){
+        var txv:TextView=findViewById(R.id.txv)
+        txv.text=(1..100).random().toString()
     }
 }
