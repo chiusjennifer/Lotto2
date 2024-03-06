360e6621703cd3a20beea3cc83139bcb20b72435
chiusjennifer <chiusjennifer@gmail.com>
2024-03-06 17:55:51 +0800

    final

--- app/src/main/java/tw/edu/pu/csce/s1114859/lotto2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csce/s1114859/lotto2/MainActivity.kt
@@ -16,7 +16,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun happy(v: View){
-        var txv:TextView=findViewById(R.id.txv)
-        txv.text=(1..100).random().toString()
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = "0"
+        }
     }
+
 }
