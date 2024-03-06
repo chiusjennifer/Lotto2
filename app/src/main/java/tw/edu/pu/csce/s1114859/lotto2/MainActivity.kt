6942c96fd463db328c06aa9ae188db412f953416
chiusjennifer <chiusjennifer@gmail.com>
2024-03-06 16:44:26 +0800

    second commit

--- app/src/main/java/tw/edu/pu/csce/s1114859/lotto2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csce/s1114859/lotto2/MainActivity.kt
@@ -1,11 +1,16 @@
 package tw.edu.pu.csce.s1114859.lotto2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txv: TextView =findViewById(R.id.txv)
+        txv.text="0"
     }
 }
